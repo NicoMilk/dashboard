4bf6e4f3a16a0a80d3ebbb9b5c8af28f14158fc5
Nico <nicomilk@gmail.com>
2020-09-16 23:37:16 +0200

    scTickets_displayTest OK

--- app-dashboard/src/components/ShowCacaoTickets.jsx
+++ app-dashboard/src/components/ShowCacaoTickets.jsx
@@ -9,12 +9,13 @@ export class ShowCacaoTickets extends Component {
     this.state = {
       userId: '',
       userToken: '',
+      isLogged: false,
       tickets: [],
     }
   }
 
   componentDidMount = () => {
-    if (this.state.userId && this.state.userToken) {
+    if (this.state.isLogged) {
       const tokenBearer = 'Bearer ' + this.state.userToken
       axios.get(`http://localhost:3002/tickets/user/${this.state.userId}`, { 'headers': { 'Authorization': tokenBearer } })
         .then(res => {
@@ -35,13 +36,45 @@ export class ShowCacaoTickets extends Component {
       const tokenBearer = 'Bearer ' + this.state.userToken
       axios.get(`http://localhost:3002/tickets/user/${this.state.userId}`, { 'headers': { 'Authorization': tokenBearer } })
         .then(res => {
-          console.log(res)
-          this.setState({ tickets: res.data })
+          this.setState({ isLogged: true })
+          this.setState({ tickets: res.data }, function () {
+            console.log("user tickets : ", this.state.tickets)
+          })
         })
     }
   }
 
   render() {
+    let display
+
+    if (!this.state.isLogged) {
+      display =
+        <div className="d-flex flex-row p-3 align-items-start">
+          <Form>
+            <Form.Control type="text" name="userId" placeholder="UserID" onChange={this.handleChange} />
+            <Form.Control type="text" name="userToken" placeholder="Token" onChange={this.handleChange} />
+            <br />
+            <button onClick={this.handleSubmit} className="btn-dark">OK</button>
+          </Form>
+        </div>
+    } else {
+      display =
+        <div className="overflow-auto px-3">
+          {
+            this.state.tickets.map((ticket) => (
+              <div>
+                <p className="text-center">
+                  {ticket.concert_id.bands}<br />
+                  {/* TODO toLocaleString not working */}
+                  {ticket.concert_id.date.toLocaleString('en-GB', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' })}<br />
+                  {ticket.concert_id.place}
+                </p>
+              </div>
+            ))
+          }
+        </div>
+    }
+
     return (
       <div>
         <Card style={{
@@ -50,27 +83,7 @@ export class ShowCacaoTickets extends Component {
         } className="shadow my-4" >
           <h5 className="text-center p-2 font-weight-bold bg-warning">Show Cacao - Tickets</h5>
 
-          <div className="d-flex flex-row p-3 align-items-start">
-            <Form>
-              <Form.Control type="text" name="userId" placeholder="UserID" onChange={this.handleChange} />
-              <Form.Control type="text" name="userToken" placeholder="Token" onChange={this.handleChange} />
-              <br />
-              <button onClick={this.handleSubmit} className="btn-dark">OK</button>
-            </Form>
-          </div>
-
-          {/* <div className="overflow-auto px-3">
-            {
-              this.state.tickets.map((ticket) => (
-                <div>
-                  <p className="text-center">
-                    {ticket.bands}<br></br>
-                    {ticket.place}
-                  </p>
-                </div>
-              ))
-            }
-          </div> */}
+          {display}
         </Card >
       </div>
     )
