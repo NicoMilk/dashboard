c9cddb1cb2572d9e7454f80dec1c949a9c109643
Nico <nicomilk@gmail.com>
2020-09-17 11:00:21 +0200

    FIX save dashboard

--- app-dashboard/src/components/Dashboard.js
+++ app-dashboard/src/components/Dashboard.js
@@ -4,56 +4,35 @@ import PropTypes from 'prop-types'
 //import axios from 'axios';
 /* import OmdbSummary from './OmdbSummary';
 import OmdbActors from './OmdbActors';
-<<<<<<< HEAD
 import News from './News'; */
 
 class Dashboard extends Component {
 
-    render() {
-
-        return <div className="d-flex flex-row flex-wrap justify-content-around align-self-start">
-            {this.props.widgets.map((widget) => {
-                const NewTag = widget.name
-                return (
-                    <div key={widget.id}>
-                        <NewTag value={widget.value} id={widget.id} deleteWidget={this.props.deleteWidget} />
-                    </div>
-
-                )
-            }
-            )}
-
-        </div>
-
-
-    }
-=======
-import News from './News';
-import ClockTime from './ClockTime';
-import ClockDate from './ClockDate';
-
-class Dashboard extends Component {
-
   render() {
-    return (
-      <div className="d-flex flex-row flex-wrap justify-content-around align-self-start">
-        <OmdbSummary />
-        <OmdbActors />
-        <News />
-        <ClockTime />
-        <ClockDate />
-      </div>
 
-    )
+    return <div className="d-flex flex-row flex-wrap justify-content-around align-self-start">
+      {this.props.widgets.map((widget) => {
+        const NewTag = widget.name
+        return (
+          <div key={widget.id}>
+            <NewTag value={widget.value} id={widget.id} deleteWidget={this.props.deleteWidget} />
+          </div>
+
+        )
+      }
+      )}
+
+    </div>
+
+
   }
->>>>>>> clock
 }
 
 
 
 Dashboard.propTypes = {
-    widgets: PropTypes.array.isRequired,
-    deleteWidget: PropTypes.func.isRequired
+  widgets: PropTypes.array.isRequired,
+  deleteWidget: PropTypes.func.isRequired
 }
 
 export default Dashboard;
