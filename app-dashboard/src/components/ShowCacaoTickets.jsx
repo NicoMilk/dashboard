45160d0da8fe3a2c6dfe6f3c96deb851c6f87b67
Nico <nicomilk@gmail.com>
2020-09-17 19:12:44 +0200

    QF scTickets options OK

--- app-dashboard/src/components/ShowCacaoTickets.jsx
+++ app-dashboard/src/components/ShowCacaoTickets.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form'
+import Dropdown from 'react-bootstrap/Dropdown';
+import * as Icon from 'react-bootstrap-icons';
 
 export class ShowCacaoTickets extends Component {
   constructor() {
@@ -11,6 +13,7 @@ export class ShowCacaoTickets extends Component {
       userToken: '',
       isLogged: false,
       tickets: [],
+      color: 'yellow',
     }
   }
 
@@ -37,16 +40,28 @@ export class ShowCacaoTickets extends Component {
       axios.get(`http://localhost:3002/tickets/user/${this.state.userId}`, { 'headers': { 'Authorization': tokenBearer } })
         .then(res => {
           this.setState({ isLogged: true })
-          this.setState({ tickets: res.data }, function () {
-            console.log("user tickets : ", this.state.tickets)
-          })
+          this.setState({ tickets: res.data })
         })
     }
   }
 
+  setColor = (e) => {
+    this.setState({ color: e.target.value })
+  }
+
   render() {
+    let color
     let display
 
+    // choose background color
+    if (this.state.color === 'yellow') {
+      color = 'bg-warning'
+    }
+    if (this.state.color === 'blue') {
+      color = 'bg-info'
+    }
+
+    //!logged = login form - logged = user's ticket list
     if (!this.state.isLogged) {
       display =
         <div className="d-flex flex-row p-3 align-items-start">
@@ -64,11 +79,11 @@ export class ShowCacaoTickets extends Component {
             this.state.tickets.map((ticket) => (
               <div>
                 <p className="text-center">
-                  {ticket.concert_id.bands}<br />
-                  {/* TODO toLocaleString not working */}
-                  {ticket.concert_id.date.toLocaleString('en-GB', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' })}<br />
+                  {ticket.concert_id.bands.join(' - ')}<br />
+                  {new Date(ticket.concert_id.date).toLocaleString('fr-FR', { year: 'numeric', month: 'long', day: '2-digit', weekday: 'long' })}<br />
                   {ticket.concert_id.place}
                 </p>
+                <hr />
               </div>
             ))
           }
@@ -81,9 +96,26 @@ export class ShowCacaoTickets extends Component {
           width: '25rem', maxHeight: "300px"
         }
         } className="shadow my-4" >
-          <h5 className="text-center p-2 font-weight-bold bg-warning">Show Cacao - Tickets</h5>
-
-          {display}
+          <div className={color + " d-flex justify-content-between p-2"}>
+            <h5 className={"text-center p-2 font-weight-bold " + color}>Show Cacao - Tickets</h5>
+            <Dropdown className="ml-auto mr-1">
+              <Dropdown.Toggle id="dropdown-basic">
+                <Icon.Tools className="" />
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item>
+                  <Form.Check type="radio" name="color" value="yellow" label="Yellow" onClick={this.setColor} checked={this.state.color === 'yellow'} />
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <Form.Check type="radio" name="color" value="blue" label="Blue" onClick={this.setColor} checked={this.state.color === 'blue'} />
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <a href=""><Icon.XSquareFill onClick={this.props.deleteWidget.bind(this, this.props.id)} color="red" size={30} className="" /></a>
+          </div>
+          <div className="overflow-auto px-3">
+            {display}
+          </div>
         </Card >
       </div>
     )
